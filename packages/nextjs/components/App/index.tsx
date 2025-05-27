e83d5825dc8829ac594d5ec688db852f96c537e7
velikanghost <velikanghost@yahoo.com>
2025-05-27 15:53:40 +0100

    refactoring

--- packages/nextjs/components/App/index.tsx
+++ packages/nextjs/components/App/index.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from "next/dynamic";
 
-const Home = dynamic(() => import("~~/components/home"), {
+const Home = dynamic(() => import("~~/components/Home"), {
   ssr: false,
   loading: () => <div>Loading...</div>,
 });
